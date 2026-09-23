c1bb9578687b2445709b212c56ba43d16f0fcf26
Zachary Michael Romualdez <zromualdez@gmail.com>
2026-09-23 20:10:14 +0800

    feat: test index route

--- src/main/java/com/pomodoro/backend/BackendApplication.java
+++ src/main/java/com/pomodoro/backend/BackendApplication.java
@@ -2,9 +2,15 @@ package com.pomodoro.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BackendApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
